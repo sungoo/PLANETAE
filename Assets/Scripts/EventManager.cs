aed598e875a6168406275d49273b614c25a13625
sungoo <qkrtjddn234@naver.com>
2026-05-02 01:31:13 +0900

    나레이터 이벤트 추가
    극초반부 나레이터 이벤트 추가

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -10,6 +10,16 @@ public class EventManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI WarpMessege;
     [SerializeField] private TextMeshProUGUI GrossyStore;
 
+    private string[] EvMessasge = {
+        "플라네타에 은하계에 도착",
+        "워프 안정성 : <color=\"yellow\">양호</color>",
+        "지금 플라네타에 은하계의 중심 행성 \n<color=\"grey\">포디나</color> 로 워프할까요?",
+        "워프 발동 준비. \n자리에서 움직이지 마십시오.",
+        "워프 안정성 : <color=\"red\">불안정</color>"
+    };
+
+    int EvMsgCnt = 0;
+
     public void StartCounting()
     {
         StartCoroutine(Counting());
@@ -36,5 +46,16 @@ public class EventManager : MonoBehaviour
             Translate.text = "자동 번역 활성화 .." + percent.ToString() + "%";
             yield return new WaitForSeconds(0.4f);
         }
+    }
+
+    public void EvCounting()
+    {
+        EvMsgCnt++;
+    }
+    public void EvMsgUpdate()
+    {
+        if (EvMsgCnt >= EvMessasge.Length) return;
+
+        WarpMessege.text = EvMessasge[EvMsgCnt];
     }
 }
