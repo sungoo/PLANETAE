c5811a49a3e296be382c19bb58568f74cf9d8b17
sungoo <qkrtjddn234@naver.com>
2026-05-09 00:32:42 +0900

    Player Move

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,14 +3,24 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour, PlayerMove.IFeildMoveActions
 {
+    private CharacterSprite PlayerSprite;
+
     private PlayerMove PlayerAction;
     private PlayerMove.FeildMoveActions Player;
+    //private CharacterController CharacterController;
+    [SerializeField] private float speed = 5f;
+    private Vector2 moveVector;
+    private bool coll;
+    [SerializeField] private Rigidbody2D rigid;
 
     private void Awake()
     {
+        PlayerSprite = GetComponent<CharacterSprite>();
         PlayerAction = new PlayerMove();
         Player = PlayerAction.FeildMove;
         Player.AddCallbacks(this);
+        //CharacterController = GetComponent<CharacterController>();
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     void OnDestroy()
@@ -28,10 +38,19 @@ public class PlayerMovement : MonoBehaviour, PlayerMove.IFeildMoveActions
         Player.Disable();
     }
 
+    private void Update()
+    {
+        //CharacterController.Move(moveVector*Time.deltaTime*speed);
+        rigid.AddForce(moveVector, ForceMode2D.Impulse);
+    }
+
     public void OnMove(InputAction.CallbackContext context)
     {
         Vector2 input = context.ReadValue<Vector2>();
-        Debug.Log(input);
+        if (context.performed)
+            moveVector = input * speed;
+        if (context.canceled)
+            moveVector = Vector2.zero;
     }
     public void OnInterect(InputAction.CallbackContext context)
     {
