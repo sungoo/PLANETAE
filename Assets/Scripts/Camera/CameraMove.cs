25f467838d7da683ecd47a765415c0c600c03137
sungoo <qkrtjddn234@naver.com>
2026-05-19 23:21:37 +0900

    Player Chasing Camera
    플레이어를 뒤따라오는 카메라
    데드존은 아직 못 만들었습니다.

---
+++ Assets/Scripts/Camera/CameraMove.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [SerializeField] private Transform Player;
+
+    [Range(0.5f, 10)]
+    [SerializeField] private float cameraSpeed = 2;
+
+    [Range(0, 1)]
+    [SerializeField] private float cameraOffset = 0.2f;
+
+    private float fixedZ = -10;
+
+    private void LateUpdate()
+    {
+        //플레이어로부터 n% 떨어진 위치로 보정되게끔 처리
+        Vector3 offset = (transform.position - Player.position) * cameraOffset;
+
+        Vector3 desire = new Vector3(
+            Player.position.x + offset.x,
+            Player.position.y + offset.y,
+            fixedZ
+        );
+        //위치 보간
+        Vector3 smooth = Vector3.Lerp(transform.position, desire, cameraSpeed*Time.deltaTime);
+        //z고정
+        smooth.z = fixedZ;
+        //위치 업데이트
+        transform.position = smooth;
+    }
+}
